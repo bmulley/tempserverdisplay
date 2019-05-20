e576233b9415d11adf605c2f1efd7af8dd4968c5
bmulley <bmulley+github@gmail.com>
2019-05-20 16:42:34 -0400

    remove filtering on SQL query

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if (mysqli_connect_errno())
         exit;
         }
 
-$result = mysqli_query($con, "Select * FROM temp WHERE id IN( SELECT max(id) from temp group by name) order by name");
+$result = mysqli_query($con, "Select * FROM temp");
 echo $result;
 ?>
 

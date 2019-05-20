b0551ca7ef9d10f7f14d4c380c8fa7c5df21335a
bmulley <bmulley+github@gmail.com>
2019-05-20 16:33:19 -0400

    add diagnostics

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 <?php
 require_once("/home/config.php");
 $con = mysqli_connect($host,$username,$password,$database);
-
+echo $con;
 if (mysqli_connect_errno())
         {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
